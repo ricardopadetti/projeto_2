7c39e864694c63ab3b2464dcaa69eb3e93090efd
Ricardo Padetti <ricardopadetti@gmail.com>
2022-09-03 17:58:14 -0300

    página final

--- main.js
+++ main.js
@@ -1,5 +1,32 @@
 const form = document.getElementById('form-contatos');
+let linhas = '';
 
 form.addEventListener('submit', function(e){
     e.preventDefault();
+
+    adicionaLinha();
+    colocaNaTabela();
 });
+
+function adicionaLinha() {
+    const inputNome = document.getElementById('nome');
+    const inputSobrenome = document.getElementById('sobrenome');
+    const inputTelefone = document.getElementById('telefone');
+
+    let linha = '<tr>';
+    linha += `<td>${inputNome.value}</td>`;
+    linha += `<td>${inputSobrenome.value}</td>`;
+    linha += `<td>${inputTelefone.value}</td>`;
+    linha += `</tr>`;
+
+    linhas += linha;
+
+    inputNome.value = '';
+    inputSobrenome.value = '';
+    inputTelefone.value = '';
+}
+
+function colocaNaTabela() {
+    const corpoTabela = document.querySelector('tbody');
+    corpoTabela.innerHTML = linhas;
+}
